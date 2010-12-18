8633e4fc5e329e1134939b9c76595e512562ddbf
Alan Creech <acreech@ironspot.com>
2010-12-18 11:35:44 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hppod::Application.config.secret_token = 'ee4477e12af7342bb95eaeae2655da6747e8a5d64fadc45e4a3e5cfbd1aa1a557433a395be3b13a4bd30cffd607882b0a15d7434d6b20b77f495e86f4b7664da'
